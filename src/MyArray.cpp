ac3cf40b29487a02947f539e2a285fedeefa01cc
hunter <hantengx@163.com>
2021-10-17 20:43:27 +0800

    加一

--- src/MyArray.cpp
+++ src/MyArray.cpp
@@ -4,6 +4,10 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
 #include "MyArray.h"
 using namespace std;
 
@@ -41,9 +45,114 @@ int MyArray::MaxProfit(vector<int> &prices) {
     return sum;
 }
 
+void MyArray::Rotate(vector<int> &nums, int k) {
+    auto len = nums.size();
+    if (len <= 1 || k % len == 0)
+    {
+        return;
+    }
+
+    auto j = 0, count = 0;
+    auto pre = 0, next = 0;
+    for (int i = 0; i < len && count < len; ++i) {
+        j = i;
+        pre = nums[j];
+        do {
+            j = (j + k) % len;
+            next = nums[j];
+            nums[j] = pre;
+            pre = next;
+            ++count;
+        } while (j != i);
+    }
+}
+
+bool MyArray::containsDuplicate(vector<int> &nums) {
+//    sort(nums.begin(), nums.end());
+//    for (int i = 1; i < nums.size(); ++i) {
+//        if (nums[i -1] == nums[i]){
+//            return true;
+//        }
+//    }
+//    return false;
+    auto len = nums.size();
+    if (len <= 1){
+        return false;
+    }
+    auto flag = new unordered_set<int>();
+    for (int i = 0; i < len; ++i) {
+        if (flag->find(nums[i]) == flag->end()){
+            flag->insert(nums[i]);
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+}
+
+int MyArray::singleNumber(vector<int> &nums) {
+    auto result = 0;
+    for (auto &num:nums) {
+        result ^= num;
+    }
+    return result;
+}
+
+vector<int> MyArray::intersect(vector<int> &nums1, vector<int> &nums2) {
+    if (nums1.size() > nums2.size()){
+        return intersect(nums2,nums1);
+    }
+    unordered_map<int, int> flag;
+    vector<int> result;
+    for (auto &num: nums1) {
+        ++flag[num];
+    }
+//    for (auto &num:nums2) {
+//        if (flag[num] > 0){
+//            --flag[num];
+//            result.push_back(num);
+//        }
+//    }
+    for (auto &num:nums2) {
+        if (flag.count(num) > 0){
+            --flag[num];
+            result.push_back(num);
+            if (flag[num] == 0){
+                flag.erase(num);
+            }
+        }
+    }
+
+    return result;
+}
+
+vector<int> MyArray::plusOne(vector<int> &digits) {
+    auto tmp = 1;
+    for (int i = digits.size() - 1; i >= 0 ; --i) {
+        digits[i] += tmp;
+        tmp = digits[i] / 10;
+        if (tmp > 0) {
+            digits[i] %= 10;
+        }else{
+            break;
+        }
+    }
+
+    if (tmp > 0){
+        digits.insert(digits.begin(), tmp);
+    }
+    return digits;
+}
+
 void MyArray::Test() {
     auto array = new MyArray();
-    auto vector = new std::vector<int>{0,1,1,2,2};
-    auto len = array->removeDuplicates(*vector);
-    cout << "array length: " << len << endl;
+//    auto vector = new std::vector<int>{0,1,1,2,2};
+//    auto len = array->removeDuplicates(*vector);
+//    auto vector = new std::vector<int>{1,2,3,4,5,6,7};
+//    array->Rotate(*vector, 3);
+    auto vector1 = new std::vector<int>{4,9,5};
+    auto vector2 = new std::vector<int>{9,4,9,8,4};
+    auto result = array->intersect(*vector1, *vector2);
+//    cout << "array length: " << len << endl;
 }
--- src/MyArray.h
+++ src/MyArray.h
@@ -15,11 +15,25 @@ public:
 //    不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
     int removeDuplicates(vector<int>& nums);
 
-    //给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。
+    //给定一个数组 prices ，其中prices[i] 是一支给定股票第 i 天的价格。
     //设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
     //注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）
     int MaxProfit(vector<int>& prices);
-
+    //给定一个数组，将数组中的元素向右移动k个位置，其中k是非负数。
+    //进阶：
+    //尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+    //你可以使用空间复杂度为O(1) 的原地算法解决这个问题吗？
+    void Rotate(vector<int>& nums, int k);
+    //给定一个整数数组，判断是否存在重复元素。
+    bool containsDuplicate(vector<int>& nums);
+    //给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+    int singleNumber(vector<int>& nums);
+    //给定两个数组，编写一个函数来计算它们的交集。
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2);
+    //给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+    //最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+    //你可以假设除了整数 0 之外，这个整数不会以零开头。
+    vector<int> plusOne(vector<int>& digits);
 };
 
 
